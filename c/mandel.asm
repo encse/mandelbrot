139d85819400a2e300b0736eb3da62ea0eb1b457
David Nemeth Csoka <encse@csokavar.hu>
2021-01-25 13:47:35 +0100

    move main program to second stage

--- c/mandel.asm
+++ c/mandel.asm
@@ -19,6 +19,75 @@ bits 16
 org 0x7c00
 %include "bpb.inc"
 
+; ----------------------------------------
+; First stage loads main program from disk
+; ----------------------------------------
+
+boot_start:  
+    xor     ax, ax
+    mov     ds, ax          
+    mov     es, ax
+    mov     ss, ax          ; Set stack pointer just below bootloader
+    mov     sp, 0x7c00
+
+    ; https://en.wikipedia.org/wiki/INT_13H#INT_13h_AH=02h:_Read_Sectors_From_Drive
+    adr_main     EQU 0x7e00
+    sector_count EQU 2
+    start_sector EQU 2
+
+    mov     bx, adr_main        ; es:bx contains the buffer address
+	mov     dh, 0x00            ; head number
+                                ; dl contains the drive number (set by bios)
+	mov     ah, 0x02            ; 2 for reading
+	mov     al, sector_count    
+	mov     ch, 0x00            ; cylinder
+	mov     cl, start_sector    
+	int     0x13
+
+    jc      .disk_read_error
+
+	cmp     al, sector_count
+	jne     .disk_read_error
+
+    jmp     mandel_start
+
+.disk_read_error:
+	mov     bx, DISK_READ_ERROR
+	call    print_string
+    jmp abort
+
+print_string:
+    pusha
+
+.loop:
+    mov     al, [bx]    ; load what `bx` points to
+    cmp     al, 0
+    je      .return
+    push    bx          ; save bx
+    mov     ah, 0x0e    ; load this every time through the loop
+                        ; you don't know if `int` preserves it
+    int     0x10
+    pop     bx          ; restore bx
+    inc     bx
+    jmp     .loop
+
+.return:
+    popa
+    ret
+
+abort:
+    hlt
+    jmp     abort  
+
+    DISK_READ_ERROR db `DISK_READ_ERROR\r\n`, 0
+
+    times 510 - ($ - $$) db 0   ;Fill the rest of sector with 0	times 510 - ($ - $$) db 0           
+    dw 0xAA55                   ;Add boot signature at the end of bootloader 
+
+; -----------------------
+; Second stage MANDELBROT
+; -----------------------
+
 data:
     x           dw 0
     y           dw 0
@@ -43,11 +112,10 @@ data:
     VGA         dw 0xa000
     screen_ptr  dw 0x0000
 
-boot_start:         
+mandel_start:         
     xor     ax, ax
     mov     ds, ax          
-    mov     ss, ax          ; Set stack pointer just below bootloader
-    mov     sp, 0x7c00
+   
     mov     es, word [VGA]
 
     mov     ax, 13h         ; Turn on graphics mode (320x200)
@@ -189,6 +257,6 @@ boot_start:
 .exit:      
     hlt                             ; Halt processor until next interrupt
     jmp     .exit
-
-    times   510 - ($ - $$) db 0     ; Fill the rest of sector with 0
-    dw      0xAA55                  ; Add boot signature at the end of bootloader
+  
+    times 8192 - ($ - $$) db 0      ;Fill the rest of sector with 0	times 510 - ($ - $$) db 0           
+  
